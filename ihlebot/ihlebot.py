fd1aeac6bb0fa7fae66b066eef30fb752a8c34f2
n1tr0-5urf3r <fabi@ihlecloud.de>
2020-08-03 16:11:43 +0200

    Added send command

--- ihlebot/ihlebot.py
+++ ihlebot/ihlebot.py
@@ -227,13 +227,18 @@ class Ihlebot(commands.Cog):
     #         except AttributeError:
     #             await self.bot.say("Fehler")
 
-
-    @commands.command(pass_context=True)
     @commands.check(user_is_me)
+    @commands.command(pass_context=True)
     async def send(self, ctx, channelId, message):
         """Sends a message to the specified channel identified by its ID"""
         channel = client.get_channel(channelId)
-        await channel.send(message)
+        if channel:
+            try:
+                await channel.send(message)
+            except:
+                await ctx.send("Cannot send a message to this channel!")
+        else:
+            await ctx.send("Channel not found")
 
 # @commands.command(pass_context=True)
     # @commands.has_role("Administrator")
